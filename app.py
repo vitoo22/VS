8b567868cb3aa35d69d6b0b70cfc13b773f75ddc
Vittorio Somaschini <vituz@MacBook-Pro.local>
2015-04-15 14:31:38 +1000

    updated app.py as per github example

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -19,4 +21,7 @@ def travels():
 def blog():
 	return render_template('index.php', active="blog")
 
-app.run(debug=True, host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
