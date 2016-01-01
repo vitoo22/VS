87288bb30620c78121a77da7ed79d8b5db5338ef
vitoo22 <vituz@VitosMacBook.local>
2016-01-01 12:50:11 +0100

    locations.js updated (Livigno)

--- static/js/locations.js
+++ static/js/locations.js
@@ -1,4 +1,5 @@
 var myLocations = [
+	["Livigno", "Italy", 46.5363, 10.13348, "car"],
 	["Milan", "Italy", 45.4667, 9.1667, "plane"],
 	["Manchester", "United Kingdom", 53.4667, -2.2333, "train"],
 	["Crewe", "United Kingdom", 53.0990, -2.4400, "car"],
