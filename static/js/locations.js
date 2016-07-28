5149fc418aff2bc7b13d74a5266d09854a59f755
vitoo22 <vituz@VitosMacBook.local>
2016-07-27 20:45:27 -0400

    27/07/2016 locations.js updated

--- static/js/locations.js
+++ static/js/locations.js
@@ -1,5 +1,6 @@
 var myLocations = [
-	["New York", "United States", 40.693432, -73.919645, "plane"],
+	["New York", "United States", , "plane"],
+	["New York", "United States", 40.7085270,-73.9405850, "plane"],
 	["Milan", "Italy", 45.4667, 9.1667, "plane"],
 	["London", "United Kingdom", 51.5072, -0.1275, "plane"],
 	["Eastbourne", "Italy", 50.7680, 0.2905, "bus"],
